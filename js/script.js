cc5d90c0c82a0f308a6676e05b795f707b4ed774
Taleh <talehqarayev@mail.ru>
2021-08-17 22:57:20 +0400

    remove menu_close for now

--- js/script.js
+++ js/script.js
@@ -1,49 +1,47 @@
-      window.addEventListener('DOMContentLoaded', () => {
-        const menu = document.querySelector('.menu'),
-        menuItem = document.querySelectorAll('.menu_item'),
-        hamburger = document.querySelector('.hamburger');
-    
-        hamburger.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        });
-    
-        menuItem.forEach(item => {
-            item.addEventListener('click', () => {
-                hamburger.classList.toggle('hamburger_active');
-                menu.classList.toggle('menu_active');
-            });
-        });
-    });
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.menu'),
+  menuLink = document.querySelectorAll('.menu_link'),
+  hamburger = document.querySelector('.hamburger');
+
+  hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('menu_active');
+  });
+
+
+
+  menuLink.forEach(item => {
+      item.addEventListener('click', () => {
+          hamburger.classList.toggle('hamburger_active');
+          menu.classList.toggle('menu_active');
+      });
+  });
+});
 
 
 
 
- 
- /*
-    const hamburger = document.querySelector(".hamburger"),
-    menu = document.querySelector(".menu"),
-    closeBtn = document.querySelector(".menu_close"),
-    menuItem = document.querySelector(".menu_item");
+/* const hamburger = document.querySelector(".hamburger"),
+menu = document.querySelector(".menu"),
+menuItem = document.querySelector(".menu_link"),
+closeBtn = document.querySelector(".menu_close");
 
 hamburger.addEventListener("click", function() {
   menu.classList.add("menu_active");
-});
-
-closeBtn.addEventListener("click", function() {
-  menu.classList.remove("menu_active");
-
 });
 
   menuItem.addEventListener("click", function() {
   menu.classList.remove("menu_active");
 }); 
 
+closeBtn.addEventListener("click", function() {
+  menu.classList.remove("menu_active");
+
+});
+
 const percents = document.querySelectorAll(".skills_rating-percent"),
     lines = document.querySelectorAll(".skills_rating-line span");
 
     percents.forEach((item, i) => {
       lines[i].style.width = item.innerHTML;
-    });
-
-    */
+    }); */
