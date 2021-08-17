f6e31bfe5a8674a949e02e8b26214ff875540a6d
Taleh <talehqarayev@mail.ru>
2021-08-17 21:24:11 +0400

    fixed js menu active bar

--- js/script.js
+++ js/script.js
@@ -1,19 +1,49 @@
-const hamburger = document.querySelector(".hamburger"),
-      menu = document.querySelector(".menu"),
-      closeBtn = document.querySelector(".menu_close");
+      window.addEventListener('DOMContentLoaded', () => {
+        const menu = document.querySelector('.menu'),
+        menuItem = document.querySelectorAll('.menu_item'),
+        hamburger = document.querySelector('.hamburger');
+    
+        hamburger.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        });
+    
+        menuItem.forEach(item => {
+            item.addEventListener('click', () => {
+                hamburger.classList.toggle('hamburger_active');
+                menu.classList.toggle('menu_active');
+            });
+        });
+    });
+
+
+
+
+ 
+ /*
+    const hamburger = document.querySelector(".hamburger"),
+    menu = document.querySelector(".menu"),
+    closeBtn = document.querySelector(".menu_close"),
+    menuItem = document.querySelector(".menu_item");
 
 hamburger.addEventListener("click", function() {
-    menu.classList.add("menu_active");
+  menu.classList.add("menu_active");
 });
 
 closeBtn.addEventListener("click", function() {
-    menu.classList.remove("menu_active");
+  menu.classList.remove("menu_active");
 
 });
 
-const percents = document.querySelectorAll(".skills_rating-percent"),
-      lines = document.querySelectorAll(".skills_rating-line span");
+  menuItem.addEventListener("click", function() {
+  menu.classList.remove("menu_active");
+}); 
 
-      percents.forEach((item, i) => {
-        lines[i].style.width = item.innerHTML;
-      });
+const percents = document.querySelectorAll(".skills_rating-percent"),
+    lines = document.querySelectorAll(".skills_rating-line span");
+
+    percents.forEach((item, i) => {
+      lines[i].style.width = item.innerHTML;
+    });
+
+    */
