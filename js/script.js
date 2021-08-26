8270c195d527859a637ad88f225e022682986c19
Шукран <talehqarayev@mail.ru>
2021-08-26 12:22:11 +0400

    added phpmailer

--- js/script.js
+++ js/script.js
@@ -26,6 +26,25 @@ $(window).scroll(function() {
         }
 });
 
+
+$("form").submit(function (e) {
+
+  e.preventDefault();
+  $.ajax({
+
+      type: "POST",
+      url:"mailer/smart.php",
+      data: $(this).serialize()
+
+    }).done(function() {
+      $(this).find("input").val("");
+
+
+      $("form").trigger("reset");
+
+    });
+    return false;
+  }); 
 });
 
 
